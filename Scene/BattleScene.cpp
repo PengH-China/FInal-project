77145b889b44b4d211db053401b2b45714a274f3
FangXinyu-0913 <fangxinyutju202009@126.com>
2021-06-09 22:28:49 +0800

    some error
    Don't trust this commit

--- Scene/BattleScene.cpp
+++ Scene/BattleScene.cpp
@@ -8,13 +8,69 @@
 #include "cocos2d.h"
 #include "BattleScene.h"
 #include"Const/Const.h"
+#include "Hero.h"
+#include"Soldier.h"
+#include"Role.h"
+
 
 using namespace cocos2d;
 
-cocos2d::Scene* BattleScene::createScene()
+//cocos2d::Scene* BattleScene::createScene()
+//{
+//	auto scene = BattleScene::createWithPhysics();
+//	log("problem1!?");
+//	auto layer = BattleScene::create();
+//	log("scene address %p\n", &scene);
+//	log("layer address %p\n", &layer);
+//
+//	//scene->addChild(layer);
+//	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+//	scene->getPhysicsWorld()->setGravity(Vec2::ZERO);
+//	//cocos2d::Ref * pSender=;
+//	return scene;
+//	//return BattleScene::create();
+//}
+cocos2d::Scene* BattleScene::createScene(TMXTiledMap* map)
 {
-	return BattleScene::create();
+	auto scene = Scene::createWithPhysics();
+	auto layer = BattleScene::create(map);
+	scene->addChild(layer);
+	scene->getPhysicsWorld()->setGravity(Vec2::ZERO);
+	return scene;
 }
+BattleScene* BattleScene::create(TMXTiledMap* map)
+{
+	auto* pRet = new(std::nothrow) BattleScene(map);
+	if (pRet)
+	{
+		if (pRet->init())
+		{
+			pRet->autorelease();
+			return pRet;
+		}
+	}
+	delete pRet;
+	pRet = nullptr;
+	return nullptr;
+}
+//BattleScene* BattleScene::create()
+//{
+//	BattleScene* pRet = new(std::nothrow) BattleScene();
+//	if (pRet)
+//	{
+//		if (pRet->init())
+//		{
+//			pRet->autorelease();
+//			return pRet;
+//		}
+//	}
+//	//else
+//	//{
+//		delete pRet;
+//		pRet = nullptr;
+//		return nullptr;
+//	//}
+//}
 
 // Print useful error message instead of segfaulting when files are not there.
 static void problemLoading(const char* filename)
@@ -26,16 +82,36 @@ static void problemLoading(const char* filename)
 // on "init" you need to initialize your instance
 bool BattleScene::init()
 {
+	log("problem2!?");
 	if (!Scene::init())
 	{
 		return false;
 	}
+	
 	auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
 	Vec2 origin = cocos2d::Director::getInstance()->getVisibleOrigin();
 
 	//背景图片 background picture
 	BattleScene::loadBackgroundMap(this);
-	BattleScene::setGidTag();
+	//BattleScene::setGidTag();
+	/*BattleScene::createWithPhysics();
+	BattleScene::getPhysicsWorld()->setGravity(Vec2::ZERO);*/
+	BattleScene::createBarrier();
+	BattleScene::addPlayer(); 
+	/*static Hero* hero = Soldier::create();
+	auto keyBoardListenerHero = EventListenerKeyboard::create();
+	keyBoardListenerHero->onKeyPressed = CC_CALLBACK_2(Hero::onKeyPressed, hero);
+	keyBoardListenerHero->onKeyReleased = CC_CALLBACK_2(Hero::onKeyReleased, hero);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(keyBoardListenerHero, this);*/
+	auto contactListener = EventListenerPhysicsContact::create();
+	log("create contactListener");
+	contactListener->onContactBegin = CC_CALLBACK_1(BattleScene::onContactBegin, this);
+	log("CC_CALLBACK_1 IS USING");
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);
+	log("addEventListener");
+	//schedule(CC_SCHEDULE_SELECTOR(BattleScene::addPlayer), 0.3f);
+	this->scheduleUpdate();
+	//log("scheduleUpdate");
 	return true;
 }
 
@@ -56,6 +132,7 @@ void BattleScene::loadBackgroundMap(cocos2d::Ref* pSender)
 
 
 
+
 void BattleScene::menuBackCallback(cocos2d::Ref* pSender)
 {
 	MenuItem* item = (MenuItem*)pSender;
@@ -67,18 +144,18 @@ void BattleScene::menuBackCallback(cocos2d::Ref* pSender)
 
 void BattleScene::setTiledMap(TMXTiledMap* map)
 {
-	m_map = map;
+	m_pMap = map;
 	/* 保存meta图层的引用 */
-	this->m_meta = m_map->getLayer("meta");
-	this->m_meta->setVisible(false);
+	//this->m_pMeta = m_pMap->getLayer("meta");
+	//this->m_pMeta->setVisible(false);
 }
 void BattleScene::setGidTag()
 {
-	Size sizeOfMap=m_map->getMapSize();
+	Size sizeOfMap=m_pMap->getMapSize();
 	log("sizeOfMap:%f %f", sizeOfMap.width-1, sizeOfMap.height-1);
 	for (int y = 0; y <= sizeOfMap.height - 1; y++) {
 		for (int x = 0; x <= sizeOfMap.width - 1; x++) {
-			int tiledGid = m_meta->getTileGIDAt(Vec2(x,y));
+			int tiledGid = m_pMeta->getTileGIDAt(Vec2(x,y));
 			/* 不为0，代表存在这个格子 */
 			if (tiledGid != 0) {
 				/*
@@ -86,7 +163,7 @@ void BattleScene::setGidTag()
 				格子是属于meta层的，但同时也是属于整个地图的，所以在获取格子的所有属性
 				时，通过格子唯一标识在地图中取得
 				*/
-				Value properties = m_map->getPropertiesForGID(tiledGid);
+				Value properties = m_pMap->getPropertiesForGID(tiledGid);
 
 				ValueMap propertiesMap = properties.asValueMap();
 
@@ -96,8 +173,8 @@ void BattleScene::setGidTag()
 					Value prop = propertiesMap.at("Collidable");
 					if (prop.asString().compare("true") == 0)
 					{
-						Sprite* gidSprite = m_meta->getTileAt(Vec2(x, y));
-						gidSprite->setName(QS::tag::kGidTag);
+						Sprite* gidSprite = m_pMeta->getTileAt(Vec2(x, y));
+						gidSprite->setName(QS::Name::kGidTag);
 						log("set (%d,%d) gid as barrier", x, y);
 					}
 					
@@ -121,3 +198,173 @@ void BattleScene::setGidTag()
 		}
         
 }
+void BattleScene::addPlayer(/*float dt*/){
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	//PolygonInfo fig;
+	static Hero* hero = Soldier::create();
+
+
+	//fig = AutoPolygon::generatePolygon(QS::kSoldier1,Rect(0, 0, QS::kSoldierWidth, QS::kSoldierHeight));
+
+	Sprite* heroSprite = Sprite::create("player.png");
+
+	if (heroSprite == nullptr)
+	{
+		log("hero picture not found");
+	}
+	else
+	{
+		log("begin init hero");
+
+		hero->bindSprite(heroSprite);
+		hero->genePhysicsBody();
+		hero->setPosition(Point(Vec2(visibleSize.width / 2 + origin.x,
+			visibleSize.height / 2 + origin.y)));
+		this->addChild(hero, 0, QS::Name::kHero);
+		log("end init hero");
+
+		//hero->rest();
+	   // BulletLayer* bulletLayer = BulletLayer::create();
+	  //  bulletLayer->retain();
+		//bulletLayer->bindHero(hero);
+	   // this->addChild(bulletLayer, 10, sk::tag::kBulletLayer);
+
+		auto keyBoardListenerHero = EventListenerKeyboard::create();
+		keyBoardListenerHero->onKeyPressed = CC_CALLBACK_2(Hero::onKeyPressed, hero);
+		keyBoardListenerHero->onKeyReleased = CC_CALLBACK_2(Hero::onKeyReleased, hero);
+		_eventDispatcher->addEventListenerWithSceneGraphPriority(keyBoardListenerHero, this);
+		//this->keyBoardListenerOne = keyBoardListenerHero;
+
+
+	}
+	//hero->retain();
+}
+
+void BattleScene::createBarrier()
+{
+	auto group = m_pMap->getObjectGroup("barrier-wall");
+	ValueVector barrierObjects = group->getObjects();
+
+	for (auto &barrier : barrierObjects)
+	{
+		ValueMap& dict = barrier.asValueMap();
+		float x = dict["x"].asFloat();
+		float y = dict["y"].asFloat();
+		float width = dict["width"].asFloat();
+		float height = dict["height"].asFloat();
+		log("%f %f %f %f", x, y, width, height);
+
+		PhysicsBody* tmpPhysicsBody = PhysicsBody::createBox(Size(width, height));
+		tmpPhysicsBody->setDynamic(false);
+		tmpPhysicsBody->setCategoryBitmask(QS::bitMask::kMapCategory);
+		tmpPhysicsBody->setCollisionBitmask(QS::bitMask::kMapCollision);
+		tmpPhysicsBody->setContactTestBitmask(QS::bitMask::kMapContact);
+
+		Sprite* tmpSprite = Sprite::create();
+		//tmpSprite->setPosition(Vec2(x, y));
+		//tmpSprite->setAnchorPoint(Vec2::ZERO);
+		//tmpSprite->setContentSize(Size(width, height));
+		tmpSprite->setPhysicsBody(tmpPhysicsBody);
+
+		this->addChild(tmpSprite, /*2*/0, QS::Name::kBarrier);
+	}
+}
+bool BattleScene::onContactBegin(PhysicsContact& contact)
+{
+	log("onContactBegin is functioning");
+	
+	auto nodeA = contact.getShapeA()->getBody()->getNode();
+	auto nodeB = contact.getShapeB()->getBody()->getNode();
+	if (nodeA != nullptr && nodeB != nullptr)
+	{
+		if ((nodeA->getName() == QS::Name::kHero && nodeB->getName() == QS::Name::kBarrier)
+			|| (nodeB->getName() == QS::Name::kHero && nodeA->getName() == QS::Name::kBarrier))
+		{
+			if (nodeA->getName() == QS::Name::kHero)
+			{
+				log("test node A is Hero");
+				auto v = nodeA->getPhysicsBody()->getVelocity();
+				v.normalize();
+				nodeA->setPosition(-50 * v);
+				nodeA->getPhysicsBody()->setVelocity(-50 * v);
+				return true;
+			}
+			else
+			{
+				log("test node B is Hero");
+				auto v = nodeB->getPhysicsBody()->getVelocity();
+				v.normalize();
+				nodeB->setPosition(-50 * v);
+				nodeB->getPhysicsBody()->setVelocity(-50 * v);
+				return true;
+			}
+		}
+		/*if ((nodeA->getName() == QS::Name::kMonster && nodeB->getName() == QS::Name::kBarrier)
+			|| (nodeB->getName() == QS::Name::kMonster && nodeA->getName() == QS::Name::kBarrier))
+		{
+			if (nodeA->getName() == QS::Name::kMonster)
+			{
+				auto v = nodeA->getPhysicsBody()->getVelocity();
+				nodeA->getPhysicsBody()->setVelocity(-v);
+				return true;
+			}
+			else
+			{
+				auto v = nodeB->getPhysicsBody()->getVelocity();
+				nodeB->getPhysicsBody()->setVelocity(-v);
+				return true;
+			}
+		}*/
+	}
+	return false;
+}
+
+//bool init()
+//{
+//	//create a static PhysicsBody
+//	auto sprite = addSpriteAtPosition(s_centre, 1);
+//	sprite->setTag(10);
+//	sprite->getPhysicsBody()->setContactTestBitmask(0xFFFFFFFF);
+//	sprite->getPhysicsBody()->setDynamic(false);
+//
+//	//adds contact event listener
+//	auto contactListener = EventListenerPhysicsContact::create();
+//	contactListener->onContactBegin = CC_CALLBACK_1(BattleScene::onContactBegin, this);
+//	_eventDispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);
+//
+//	schedule(CC_SCHEDULE_SELECTOR(PhysicsDemoCollisionProcessing::tick), 0.3f);
+//	return true;
+//
+//	return false;
+//}
+//
+//void tick(float dt)
+//{
+//	auto sprite1 = addSpriteAtPosition(Vec2(s_centre.x + cocos2d::random(-300, 300),
+//		s_centre.y + cocos2d::random(-300, 300)));
+//	auto physicsBody = sprite1->getPhysicsBody();
+//	physicsBody->setVelocity(Vec2(cocos2d::random(-500, 500), cocos2d::random(-500, 500)));
+//	physicsBody->setContactTestBitmask(0xFFFFFFFF);
+//}
+//
+//bool onContactBegin(PhysicsContact& contact)
+//{
+//	auto nodeA = contact.getShapeA()->getBody()->getNode();
+//	auto nodeB = contact.getShapeB()->getBody()->getNode();
+//
+//	if (nodeA && nodeB)
+//	{
+//		if (nodeA->getTag() == 10)
+//		{
+//			nodeB->removeFromParentAndCleanup(true);
+//		}
+//		else if (nodeB->getTag() == 10)
+//		{
+//			nodeA->removeFromParentAndCleanup(true);
+//		}
+//	}
+//
+//	//bodies can collide
+//	return true;
+//}
--- Scene/BattleScene.h
+++ Scene/BattleScene.h
@@ -1,7 +1,7 @@
 /**
 *@file BattleScene.h
-*@author 张子涵
-*@date 6/1/2021
+*@author 张子涵 方新宇
+*@date 6/5/2021
 */
 
 
@@ -17,24 +17,38 @@ class BattleScene : public cocos2d::Scene
 {
 
 public:
-    static Scene* createScene();
 
-    virtual bool init();
+    BattleScene(TMXTiledMap* map) : m_pMap(map) { };
+
+	static Scene* createScene(TMXTiledMap* map);
+
+	static BattleScene* create(TMXTiledMap* map);
+
+    //BattleScene(TMXTiledMap* map) : m_pMap(map) { };
+    //BattleScene* createWithPhysics();
+    //static BattleScene* create();
+    //static Scene* createScene();
+
+    virtual bool init() override;
 
     //load bakground picture
     void loadBackgroundMap(cocos2d::Ref* pSender);
     //init m_map and m_map
     void BattleScene::setTiledMap(TMXTiledMap* map);
-    
     //back to menu
     void menuBackCallback(cocos2d::Ref* pSender);
     //set each gid a tag name
     void setGidTag();
     // implement the "static create()" method manually
-    CREATE_FUNC(BattleScene);
+    void addPlayer(/*float dt*/);
+
+    void createBarrier();
+    //CREATE_FUNC(BattleScene);
+    bool BattleScene::onContactBegin(PhysicsContact& contact);
 protected:
-    TMXTiledMap* m_map;
-    TMXLayer* m_meta;
+    TMXTiledMap* m_pMap=nullptr;
+    TMXLayer* m_pMeta=nullptr;
+    //static EventListenerKeyboard* keyBoardListenerOne;
 };
 
 #endif
